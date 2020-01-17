dec5cd712f1d706e5007602e26cb57c6c2e9be54
mmichel122 <mikael.michelier@googlemail.com>
2020-01-17 14:58:17 +0000

    new version added

---
+++ img/gpu.py
@@ -0,0 +1,6 @@
+def hello():
+    for i in range(25):
+        print(f"{i} - GPU Docker")
+
+if __name__ == "__main__":
+    hello()
---
+++ img/vm.py
@@ -0,0 +1,6 @@
+def hello():
+    for i in range(25):
+        print(f"{i} - VM Docker")
+
+if __name__ == "__main__":
+    hello()
